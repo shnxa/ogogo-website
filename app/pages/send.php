9c62384699e0fdd57693bc44c460308f27891c49
shnxa <shahnozataalaibekova@gmail.com>
2021-10-10 14:42:32 +0600

    'changes'

--- app/pages/send.php
+++ app/pages/send.php
@@ -1,24 +1,31 @@
 <?php
 
+$token = "2011190964:AAHnySuPv48u2aQeSiAjkWZj9LtV4fd5pzc";
 
-$token = 2011190964:AAHnySuPv48u2aQeSiAjkWZj9LtV4fd5pzc;
-
-$chat_id = 446113749;
-
+$chat_id = "446113749";
 if ($_POST['act'] == 'order') {
     $name = ($_POST['name']);
     $phone = ($_POST['phone']);
-}
 
-foreach($arr as $key => $value) {
+
+    $arr = array(
+        'Имя:' => $name,
+        'Телефон:' => $phone
+    );
+
+    foreach($arr as $key => $value) {
         $txt .= "<b>".$key."</b> ".$value."%0A";
     };
 
+
     $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
 
     if ($sendToTelegram) {
         alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
     }
+
+
     else {
         alert('Что-то пошло не так. ПОпробуйте отправить форму ещё раз.');
     }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementsByClassName("modalWindow")[0];
 const showModal = document.getElementsByClassName("dropdownBtn")[0];
 const closeModal = document.getElementById("modalBtn");
+const formWindow = document.getElementById('application-form-window')[0];
 
 
 function toggleModal () {
@@ -13,45 +14,14 @@ function ModalOpen(event) {
     }
 }
 
+setTimeout(function(){
+    formWindow.classList.add('showModal')
+},20000)
+
 
 showModal.addEventListener("click", toggleModal);
 closeModal.addEventListener("click", toggleModal);
 modal.addEventListener("click", ModalOpen);
-
-
-const slider = document.getElementsByClassName("sliders")[0];
-const buttonRight = document.getElementById("slider-btn-right");
-const buttonLeft = document.getElementById("slider-btn-left");
-
-
-
-let initCount = 0;
-
-function showLastSlide () {
-    initCount = 1
-
-    if(initCount > 0){
-        slider.classList.add('slidesToLeft')
-    }
-}
-
-function showFirstSlide () {
-    initCount = -1
-
-    if(initCount < 0) {
-        slider.classList.remove('slidesToLeft')
-        slider.classList.add('slidesToRight')
-    }
-}
-
-
-
-
-buttonLeft.addEventListener("click", showFirstSlide);
-buttonRight.addEventListener("click", showLastSlide);
-
-
-
 
 
 const accordion = document.getElementsByClassName('qa-container');
@@ -61,3 +31,48 @@ for (i=0; i<accordion.length; i++) {
     this.classList.toggle('active')
   })
 }
+
+const applicationForm = document.getElementById('application-form-window');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate()+1)
+    const deadline = new Date(tomorrow);
+    
+    let timerId = null;
+    
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      
+      let hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      let minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+
+ 
+
+    }
+    let $hours = document.querySelector('.timer__hours');
+    let $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    
+    countdownTimer();
+    
+    timerId = setInterval(countdownTimer, 1000);
+  });
+
+  
+
+ 

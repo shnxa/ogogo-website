212c80586341aac72be15ebeb1c1be5df549184a
shnxa <shahnozataalaibekova@gmail.com>
2021-10-13 16:09:24 +0600

    commit

--- production/app/pages/pages.js
+++ production/app/pages/pages.js
@@ -1,1 +1,61 @@
-const modal=document.getElementsByClassName("modalWindow")[0],showModal=document.getElementsByClassName("dropdownBtn")[0],closeModal=document.getElementById("modalBtn"),burger=document.getElementsByClassName("burger")[0];function toggleModal(){modal.classList.toggle("showModal")}function ModalOpen(e){e.target===modal&&toggleModal()}showModal.addEventListener("click",toggleModal),closeModal.addEventListener("click",toggleModal),modal.addEventListener("click",ModalOpen),burger.addEventListener("click",toggleModal),$(".telegram-form").on("submit",(function(e){e.stopPropagation(),e.preventDefault();let t=this,o=($(".submit",t),new FormData),a=$("input[type=file]");return $(".submit",t).val("Отправка..."),$("input, textarea",t).attr("disabled",""),o.append("Пробный урок",t.val()),o.append("name",$('[name="name"]',t).val()),o.append("phone",$('[name="phone"]',t).val()),a.each((function(e,t){let a=t.files;a&&$.each(a,(function(e,t){o.append(e,t)}))})),$.ajax({url:"ajax.php",type:"POST",data:o,cache:!1,dataType:"json",processData:!1,contentType:!1,xhr:function(){let e=$.ajaxSettings.xhr();return e.upload&&e.upload.addEventListener("progress",(function(e){if(e.lengthComputable){let o=e.loaded/e.total*100;o=o.toFixed(0),$(".submit",t).html(o+"%")}}),!1),e},error:function(e,t){alert("error")},complete:function(){console.log("Complete"),t.reset()}}),!1})),document.querySelector("#send2").onclick=function(){let e=document.querySelector("#applicationid"),t=document.querySelector("#phone2").value,o=[e,document.querySelector("#name2").value,t];let a=new XMLHttpRequest;a.open("GET","http://api.telegram.org/bot2094930216:AAGvtGVVJqCqCsUTLfDsYBRtWIn3a9g5ZA0/sendMessage?chat_id=-567726567&text="+o,!0),a.send()};
+const modal = document.getElementsByClassName("modalWindow")[0],
+    showModal = document.getElementsByClassName("dropdownBtn")[0],
+    closeModal = document.getElementById("modalBtn"),
+    burger = document.getElementsByClassName("burger")[0];
+function toggleModal() {
+    modal.classList.toggle("showModal");
+}
+function ModalOpen(e) {
+    e.target === modal && toggleModal();
+}
+showModal.addEventListener("click", toggleModal),
+    closeModal.addEventListener("click", toggleModal),
+    modal.addEventListener("click", ModalOpen),
+    burger.addEventListener("click", toggleModal)
+  
+
+const formWindow = document.getElementsByClassName("application-form-window")[0];
+      openWindow = document.getElementById('openForm');
+      closeForm = document.getElementById('closeForm');
+
+      function toggleForm () {
+          formWindow.classList.toggle('show-form-window');
+      }
+      function openForm (e){
+          e.target === formWindow && toggleForm();
+      }
+
+      console.log(formWindow)
+
+      openWindow.addEventListener('click', toggleForm);
+      formWindow.addEventListener('click', openForm);
+      closeForm.addEventListener('click',toggleForm );
+
+
+
+    (document.querySelector("#send").onclick = function () {
+        let e = document.querySelector("#courseTitle"),
+            t = document.querySelector("#phone").value,
+            v = document.querySelector("#name").value,
+            o = [e,v, t];
+        let a = new XMLHttpRequest();
+        a.open("GET", "http://api.telegram.org/bot2094930216:AAGvtGVVJqCqCsUTLfDsYBRtWIn3a9g5ZA0/sendMessage?chat_id=-567726567&text=" + o, !0), a.send();
+        
+        document.querySelector("#phone2").value = '';
+        document.querySelector("#name2").value = '';
+
+    });
+
+    document.querySelector('#senddata').onclick = function () {
+        let phone = document.querySelector('#phonedata').value;
+        let name = document.querySelector("#namedata").value;
+        let t = document.querySelector('#applicationid');
+        let o = [t, name, phone];
+        let a = new XMLHttpRequest();
+
+        a.open("GET", "http://api.telegram.org/bot2094930216:AAGvtGVVJqCqCsUTLfDsYBRtWIn3a9g5ZA0/sendMessage?chat_id=-567726567&text=" + o, !0), a.send();
+        
+          
+        document.querySelector("#phonedata").value = '';
+        document.querySelector("#namedata").value = '';
+    }
